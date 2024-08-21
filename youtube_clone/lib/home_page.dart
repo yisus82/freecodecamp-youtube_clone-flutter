43df85ab3f05433c06c9e03ed80ddd25f8147e2c
yisus82 <djalma8@gmail.com>
2024-08-21 16:36:28 +0200

    Remove unused variable

--- youtube_clone/lib/home_page.dart
+++ youtube_clone/lib/home_page.dart
@@ -9,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
